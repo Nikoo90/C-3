832f73fb1c5914b82a331b19ec8424e187d2411e
Nikoo90 <66570980+Nikoo90@users.noreply.github.com>
2022-09-22 22:12:24 +0300

    table of cubes of numbers from 1 to N

---
+++ 3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i <= num)
+{
+    if (i == num) Console.WriteLine($"{Math.Pow(i, 3)}.");
+
+    else Console.Write($"{Math.Pow(i, 3)},");
+
+    i++;
+}
