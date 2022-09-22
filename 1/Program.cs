146e3e668e8c4a4d2529dd769fc10c5c4b9ed728
Nikoo90 <66570980+Nikoo90@users.noreply.github.com>
2022-09-22 23:32:33 +0300

    palindrome

---
+++ 1/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] numsArr = new int[5];
+int x = 10000;
+for (int i = 0; i < numsArr.Length; i++)
+{
+    if (i == 0)
+    {
+        numsArr[i] = (num - num % x) / x;
+    }
+    else
+    {
+        int result = num % x;
+        x /= 10;
+        result /= x;
+        numsArr[i] = result;
+    }
+}
+int j = 0;
+string answer = "Yes";
+
+while (j < numsArr.Length)
+{
+    if (numsArr[j] != numsArr[numsArr.Length - (j + 1)])
+    {
+        answer = "No";
+        break;
+    }
+    j++;
+}
+Console.WriteLine(answer);
