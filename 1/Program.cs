ba4d27c2249a43dd37023ed942c484c206a57c81
Nikoo90 <66570980+Nikoo90@users.noreply.github.com>
2022-09-22 23:43:42 +0300

    Fix bug

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,31 +1,35 @@
-﻿Console.Write("Enter number: ");
+﻿Console.Write("Enter a five-digit number: ");
 int num = Convert.ToInt32(Console.ReadLine());
-int[] numsArr = new int[5];
-int x = 10000;
-for (int i = 0; i < numsArr.Length; i++)
+if (num < 10000 || num > 100000) Console.WriteLine("Not a five digit number");
+else
 {
-    if (i == 0)
+    int[] numsArr = new int[5];
+    int x = 10000;
+    for (int i = 0; i < numsArr.Length; i++)
     {
-        numsArr[i] = (num - num % x) / x;
+        if (i == 0)
+        {
+            numsArr[i] = (num - num % x) / x;
+        }
+        else
+        {
+            int result = num % x;
+            x /= 10;
+            result /= x;
+            numsArr[i] = result;
+        }
     }
-    else
-    {
-        int result = num % x;
-        x /= 10;
-        result /= x;
-        numsArr[i] = result;
-    }
-}
-int j = 0;
-string answer = "Yes";
+    int j = 0;
+    string answer = "Yes";
 
-while (j < numsArr.Length)
-{
-    if (numsArr[j] != numsArr[numsArr.Length - (j + 1)])
+    while (j < numsArr.Length)
     {
-        answer = "No";
-        break;
+        if (numsArr[j] != numsArr[numsArr.Length - (j + 1)])
+        {
+            answer = "No";
+            break;
+        }
+        j++;
     }
-    j++;
+    Console.WriteLine(answer);
 }
-Console.WriteLine(answer);
