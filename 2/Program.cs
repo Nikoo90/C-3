8aeefa8510455cb36a2f0fb51f97fd27a642cabe
Nikoo90 <66570980+Nikoo90@users.noreply.github.com>
2022-09-22 23:59:10 +0300

    Find the distance between 2 points in 3D space

---
+++ 2/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] arr = new int[6];
+for (int i = 0; i < 6; i++)
+{
+    if (i < 3) Console.WriteLine("Введите кординаты А: ");
+    else Console.WriteLine("Введите кординаты B: ");
+
+    int num = Convert.ToInt32(Console.ReadLine());
+    arr[i] = num;
+}
+double result = Math.Sqrt(
+                            Math.Pow((arr[3] - arr[0]), 2)
+                            + Math.Pow((arr[4] - arr[1]), 2)
+                            + Math.Pow((arr[5] - arr[2]), 2)
+                        );
+Console.WriteLine(Math.Round(result, 2));
